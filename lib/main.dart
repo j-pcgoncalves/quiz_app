21dcb7b3eebefed79315f51601ce655b20b64d85
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-11-17 19:37:08 +0000

    add buttons

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,27 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\s your favorite color?',
+      'What\s your favorite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Quiz App'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(children: [
+          Text('The question!'),
+          ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1')),
+          ElevatedButton(onPressed: answerQuestion, child: Text('Answer 2')),
+          ElevatedButton(onPressed: answerQuestion, child: Text('Answer 3')),
+        ]),
       ),
     );
   }
