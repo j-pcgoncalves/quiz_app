254b257713d90c8307ef0cae78cadcf72fb44767
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-11-17 19:06:53 +0000

    add and edit Scaffold widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,13 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz App'),
+        ),
+        body: Text('This is my default text!'),
+      ),
+    );
   }
 }
