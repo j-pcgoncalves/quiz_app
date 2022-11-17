3f6efd74843422b0117ba5d1609d6b6dcd83ff52
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-11-17 19:58:56 +0000

    create answerQuestion function

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,18 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
   void answerQuestion() {
-    print('Answer 1 chosen!');
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'What\s your favorite color?',
-      'What\s your favorite animal?'
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
     ];
 
     return MaterialApp(
@@ -26,7 +29,7 @@ class MyApp extends StatelessWidget {
           title: Text('Quiz App'),
         ),
         body: Column(children: [
-          Text('The question!'),
+          Text(questions[questionIndex]),
           ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1')),
           ElevatedButton(
               onPressed: () => print('Answer 2 chosen!'),
