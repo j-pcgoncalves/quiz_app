36de1f34bf3b573056e5db274fee8e4116ab0e15
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-11-18 22:24:43 +0000

    add a restart button

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,14 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   final _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
@@ -72,7 +80,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(_totalScore),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
